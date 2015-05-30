06288a5a1a013f1ea5901f80188c8ccb27dfdfeb
Matt Isenhower <matt@isenhower.com>
2015-05-30 11:43:25 -0700

    Add basic XBF parsing functionality

--- XbfAnalyzer/MainWindow.xaml.cs
+++ XbfAnalyzer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XbfAnalyzer.Xbf;
 
 namespace XbfAnalyzer
 {
@@ -36,6 +37,95 @@ namespace XbfAnalyzer
             string[] droppedItems = e.Data.GetData(DataFormats.FileDrop) as string[];
             if (droppedItems == null || droppedItems.Length < 1)
                 return;
+
+            ClearLog();
+
+            // Process each file
+            foreach (var path in droppedItems)
+                ProcessFile(path);
+        }
+
+        private void ProcessFile(string path)
+        {
+            LogBoldLine();
+            LogMessage("File: " + path);
+
+            try
+            {
+                var xbf = new XbfReader(path);
+
+                // Header info
+                LogMessage("File version: {0}.{1}", xbf.Header.MajorFileVersion, xbf.Header.MinorFileVersion);
+                LogMessage("Metadata Size: {0} (0x{0:X})", xbf.Header.MetadataSize);
+                LogMessage("Node Size: {0} (0x{0:X})", xbf.Header.NodeSize);
+                // The offset values seem to be off by 12 bytes compared to the actual positions of these elements in the XBF files.
+                // Displaying these as adjusted values to make external analysis easier.
+                LogMessage("Adjusted String Table Offset: {0} (0x{0:X})", xbf.Header.StringTableOffset + 12);
+                LogMessage("Adjusted Assembly Table Offset: {0} (0x{0:X})", xbf.Header.AssemblyTableOffset + 12);
+                LogMessage("Adjusted Type Namespace Table Offset: {0} (0x{0:X})", xbf.Header.TypeNamespaceTableOffset + 12);
+                LogMessage("Adjusted Type Table Offset: {0} (0x{0:X})", xbf.Header.TypeTableOffset + 12);
+                LogMessage("Adjusted Property Table Offset: {0} (0x{0:X})", xbf.Header.PropertyTableOffset + 12);
+                LogMessage("Adjusted XML Namespace Table Offset: {0} (0x{0:X})", xbf.Header.XmlNamespaceTableOffset + 12);
+                LogMessage();
+
+                // String table
+                LogLine();
+                LogMessage("String table:");
+                for (int i = 0; i < xbf.StringTable.Length; i++)
+                    LogHexAddressMessage(i, xbf.StringTable[i]);
+                LogMessage();
+
+                // Assembly table
+                LogLine();
+                LogMessage("Assembly table:");
+                for (int i = 0; i < xbf.AssemblyTable.Length; i++)
+                {
+                    var assembly = xbf.AssemblyTable[i];
+                    LogHexAddressMessage(i, "{0} (Kind: {1})", assembly.Name, assembly.Kind);
+                }
+                LogMessage();
+
+                // Type namespace table
+                LogLine();
+                LogMessage("Type namespace table:");
+                for (int i = 0; i < xbf.TypeNamespaceTable.Length; i++)
+                {
+                    var typeNamespace = xbf.TypeNamespaceTable[i];
+                    LogHexAddressMessage(i, "{0} (Assembly: {1})", typeNamespace.Name, typeNamespace.Assembly.Name);
+                }
+                LogMessage();
+
+                // Type table
+                LogLine();
+                LogMessage("Type table:");
+                for (int i = 0; i < xbf.TypeTable.Length; i++)
+                {
+                    var type = xbf.TypeTable[i];
+                    LogHexAddressMessage(i, "{0} (Namespace: {1}, Flags: {2})", type.Name, type.Namespace.Name, type.Flags);
+                }
+                LogMessage();
+
+                // Property table
+                LogLine();
+                LogMessage("Property table:");
+                for (int i = 0; i < xbf.PropertyTable.Length; i++)
+                {
+                    var property = xbf.PropertyTable[i];
+                    LogHexAddressMessage(i, "{0} (Type: {1}, Flags: {2})", property.Name, property.Type.Name, property.Flags);
+                }
+                LogMessage();
+
+                // XML namespace table
+                LogLine();
+                LogMessage("XML namespace table:");
+                for (int i = 0; i < xbf.XmlNamespaceTable.Length; i++)
+                    LogHexAddressMessage(i, xbf.XmlNamespaceTable[i]);
+                LogMessage();
+            }
+            catch (Exception ex)
+            {
+                LogMessage("Error: " + ex.ToString());
+            }
         }
 
         #region Message Logging
@@ -68,6 +158,11 @@ namespace XbfAnalyzer
         private void LogHexAddressMessage(int address, string message)
         {
             LogMessage("{0:X4}: {1}", address, message);
+        }
+
+        private void LogHexAddressMessage(int address, string format, params object[] args)
+        {
+            LogHexAddressMessage(address, string.Format(format, args));
         }
 
         #endregion
---
+++ XbfAnalyzer/Xbf/XbfReader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XbfAnalyzer.Xbf
+{
+    public class XbfReader
+    {
+        public XbfReader(string path)
+        {
+            using (var fileStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new BinaryReader(fileStream, Encoding.Unicode))
+            {
+                Header = new XbfHeader(reader);
+                ReadStringTable(reader);
+                AssemblyTable = ReadTable(reader, r => new XbfAssembly(this, r));
+                TypeNamespaceTable = ReadTable(reader, r => new XbfTypeNamespace(this, r));
+                TypeTable = ReadTable(reader, r => new XbfType(this, r));
+                PropertyTable = ReadTable(reader, r => new XbfProperty(this, r));
+                XmlNamespaceTable = ReadTable(reader, r => StringTable[r.ReadInt32()]);
+            }
+        }
+
+        public XbfHeader Header { get; private set; }
+        public string[] StringTable { get; private set; }
+        public XbfAssembly[] AssemblyTable { get; private set; }
+        public XbfTypeNamespace[] TypeNamespaceTable { get; private set; }
+        public XbfType[] TypeTable { get; private set; }
+        public XbfProperty[] PropertyTable { get; private set; }
+        public string[] XmlNamespaceTable { get; private set; }
+
+        private string ReadString(BinaryReader reader)
+        {
+            return new string(reader.ReadChars(reader.ReadInt32()));
+        }
+
+        private void ReadStringTable(BinaryReader reader)
+        {
+            int stringCount = reader.ReadInt32();
+            string[] stringTable = new string[stringCount];
+
+            bool isXbfV2 = Header.MajorFileVersion >= 2;
+
+            for (int i = 0; i < stringCount; i++)
+            {
+                stringTable[i] = ReadString(reader);
+
+                // XBF v2 files appear to have two extra bytes after each string.
+                // TODO: Determine if these bytes have any purpose.
+                if (isXbfV2)
+                    reader.ReadBytes(2);
+            }
+
+            StringTable = stringTable;
+        }
+
+        private T[] ReadTable<T>(BinaryReader reader, Func<BinaryReader, T> objectGenerator)
+        {
+            int count = reader.ReadInt32();
+            T[] result = new T[count];
+
+            for (int i = 0; i < count; i++)
+                result[i] = objectGenerator(reader);
+
+            return result;
+        }
+    }
+}
